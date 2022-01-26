681c47a5bc30b2cfad09d440728146c287159f9c
hozeren <denizozeren614@gmail.com>
2022-01-26 17:19:38 +0100

    favorite.py started

---
+++ bin/favorite.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import random
+import time
+
+from lxml.html import fromstring
+import nltk, sys, requests
+nltk.download('punkt', quiet=True)
+from twython import Twython, TwythonError
+import sys, requests
+
+
+class Favorite():
+
+
+    def search(keywords)
+        results = twitter.cursor(twitter.search, q=keywords)
+        for result in results:
+        print(result['id_str'])
