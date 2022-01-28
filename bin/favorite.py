a1db1116918567f49bdf6c9da75aee1b7b68f609
hozeren <denizozeren614@gmail.com>
2022-01-28 01:25:51 +0100

    test

--- bin/favorite.py
+++ bin/favorite.py
@@ -7,15 +7,25 @@ import nltk, sys, requests
 nltk.download('punkt', quiet=True)
 from twython import Twython, TwythonError
 import sys, requests
+from theaterAATW.auth import (
+    apiKey,
+    apiSecret,
+    accessToken,
+    accessTokenSecret,
+    HEADERS
+)
 
-
+keywords = ['theater']
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 
 class Favorite():
 
 
     def search_twitter(keywords):
-        results = api.cursor(twitter.search, q=keywords)
+        results = api.cursor(api.search, q=keywords)
         for result in results:
             print(result['id_str'])
             return results
+
+Favorite()
+print (Favorite.search_twitter(keywords))
