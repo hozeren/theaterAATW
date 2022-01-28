d534b7d296a5a1cac9e7465370b9a45083a12c7b
hozeren <denizozeren614@gmail.com>
2022-01-28 01:03:05 +0100

    tests update

--- tests/test_links.py
+++ tests/test_links.py
@@ -2,6 +2,8 @@
 import pytest
 from theaterAATW.modules.nytimes import scrape_nytimes
 from theaterAATW.modules.wostage import scrape_wostage
+from theaterAATW.modules.tmania import scrape_tmania
+from theaterAATW.modules.ttimes import scrape_ttimes
 
 def test_scrape_nytimes():
     nytimes_text = scrape_nytimes()
@@ -12,3 +14,8 @@ def test_scrape_wostage():
     wostage_text = scrape_wostage()
     for text in wostage_text:
         assert text, "Wostage texts works!"
+
+def test_scrape_mania():
+    texts = scrape_tmania()
+    for text in texts:
+        assert text, "TheaterMania texts works!"
